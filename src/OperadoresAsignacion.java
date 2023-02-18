c794793228973b4107b95832d16d88ebedcf10c1
KernelByte <yonihermelendez@gmail.com>
2023-02-17 23:12:10 -0500

    Archivos generados de ejecucion main para clase sobre operadores

---
+++ src/OperadoresAsignacion.java
@@ -0,0 +1,33 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+        //Operadores de asignacion
+        /*
+        * Operador   Aplicacion   Desglose
+        *    +=        a += b       a = a + b
+        *    -=        a -= b       a = a - b
+        *    *=        a *= b       a = a * b
+        *    /=        a /= b       a = a / b
+        *    %=        a %= b       a = a % b
+        *
+        * */
+
+        // Operadores
+        int vidas = 4;
+        vidas = vidas - 1;
+        System.out.println("Numero de vidas es: "+vidas);
+
+        vidas--; // Decremento
+        System.out.println("Numero de vidas es: "+vidas);
+
+        vidas++; // Incremento
+        System.out.println("Numero de vidas es: "+vidas);
+        
+        int bonus = 100 + vidas++; // Posfijo
+        System.out.println("Numero de bonus es: "+bonus);
+
+        int herramientas = 100 + ++vidas; // Prefijo
+        System.out.println("Numero de bonus es: "+herramientas);
+
+
+    }
+}
