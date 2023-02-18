56923f972b6c26c8729bf43bff9a50a046a1fc88
KernelByte <yonihermelendez@gmail.com>
2023-02-17 19:15:31 -0500

    Se añade clase sobre tipos de variables

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,15 @@
 public class Variables {
+    public static void main(String[] args) {
+        // Variable de tipo Numerico entero
+        int speed = 0; // Asignacion de valor
+        // Variable de tipo Cadena
+        String salario = "1000000";
+        // Variable de tipo numerico decimal
+        double altura = 1.75;
+        // Variable de tipo condicional
+        boolean sentencia = true;
+
+        // Impresion en consola de las anteriores variables
+        System.out.println("El valor de las variables es: Speed="+speed+" Salario="+salario);
+    }
 }
