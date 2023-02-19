4025c28c98f62d9f8efc9e6303c9ac462cbd4af6
KernelByte <yonihermelendez@gmail.com>
2023-02-19 11:03:48 -0500

    Se añade clase sobre instanciar un objeto y modificacion del metodo constructor

---
+++ src/Doctor.java
@@ -0,0 +1,9 @@
+public class Doctor {
+    public int edad;
+    public String nombre;
+    public String especialidad;
+
+    Doctor(){
+        this.edad = 30;
+    }
+}
--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,14 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        // Instancia de una clase parar crear un objeto java
+        Doctor doctor = new Doctor();
+        doctor.nombre = "Estiven Lopez";
+        //doctor.edad = 25; -- Valor inicializado en el metodo constructor del objeto
+        doctor.especialidad = "Cirujano";
+
+        //Imprimir datos del objeto
+        System.out.println("Los datos del doctor creado son: Nombre="+doctor.nombre+" Edad="+doctor.edad+" Especialidad="+doctor.especialidad);
     }
 }
