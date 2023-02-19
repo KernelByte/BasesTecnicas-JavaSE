97939ec027bed2066189713271d83bee0b3282c7
KernelByte <yonihermelendez@gmail.com>
2023-02-18 22:58:59 -0500

    Se da clase sobre las funciones en java

---
+++ src/Funciones.java
@@ -0,0 +1,10 @@
+public class Funciones {
+    public static int sumarNumeros(int a, int b){
+        int resultado = 0;
+        return resultado = a + b;
+    }
+    public static void main(String[] args) {
+       int ejecucionFuncion = sumarNumeros(3,4);
+        System.out.println("El resultado es: "+ejecucionFuncion);
+    }
+}
