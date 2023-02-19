bd49335a900234c0543048eb398bfd51a4c81750
KernelByte <yonihermelendez@gmail.com>
2023-02-18 21:19:46 -0500

    Se añade clase de operaciones matematicas

---
+++ src/OperacionesMatematicas.java
@@ -0,0 +1,19 @@
+public class OperacionesMatematicas {
+
+    public static void main(String[] args) {
+        double a = 20.5;
+        double b = 15;
+        // Devuelve un entero hacia arriba
+        System.out.println(Math.ceil(a));
+        // Devuelve un entero hacia abajo
+        System.out.println(Math.floor(a));
+        // Devuelve un numero elevado al otro
+        System.out.println(Math.pow(a,b));
+        // Devuelve el valor mas grande entre dos valores
+        System.out.println(Math.max(a,b));
+        // Devuelve la raiz cuadrada de un numero
+        System.out.println(Math.sqrt(b));
+        // Devuelve el area de un circulo
+        System.out.println(Math.PI * Math.pow(a,2));
+    }
+}
