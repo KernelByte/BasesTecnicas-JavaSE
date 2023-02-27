32bd019c99c775cecce50b2fcd2663dfe1942239
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-27 13:56:26 -0500

    Se añade conexion a la base de datos mysql

--- Persistencia de datos/mensajes_app/src/main/java/inicio.java
+++ Persistencia de datos/mensajes_app/src/main/java/inicio.java
@@ -1,5 +1,13 @@
+import java.sql.Connection;
+
 public class inicio {
     public static void main(String[] args) {
+        Conexion connection = new Conexion();
 
+        try(Connection cnx = connection.get_connection()) {
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
